07e4036a1a4694aadfa6e8a6eb533db0d5d8a8b3
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-11 18:03:03 +0300

    remove overflow property to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,26 @@
+import Form from "../components/Form"
 import ProductList from "../components/ProductList"
+import Table from "../components/Table"
 
 
-const Home = () =>{
+const Home = () => {
+ 
+    // const {cart, addProduct,clear,deleteProduct}= useCart()
 
-    return <div style={{  overflow: 'hidden'}}>
-        <ProductList />
+    
+    return <div >
+
+
+        <Table
+         headings={['Title', 'Price', 'Quantity' ]}
+         items={[
+            {title:"Macbook", price:44,quantity:4},
+            {title:"Macbook", price:44,quantity:4},
+            {title:"Macbook", price:44,quantity:4},
+
+         ]}
+         />
+         <Form />
     </div>
 }
 
