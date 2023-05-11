fee68052340d55ea22ddfe9d31945179b37c57a9
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-11 20:26:20 +0300

    Change pathes

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import { CartProvider } from './hooks/useCart';
 import CartPage from "./pages/CartPage";
-import { useEffect, useState } from "react";
+
 
 
 const Router = () => {
