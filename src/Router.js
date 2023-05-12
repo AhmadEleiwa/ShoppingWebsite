80be8c24cbb09fb2b037deb8bf78b0f13e991e8f
ahmadeleiwa <ahmadilawa@gmail.com>
2023-05-12 10:51:45 +0300

    Wrapping the site into Layout component

--- src/Router.js
+++ src/Router.js
@@ -2,18 +2,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import { CartProvider } from './hooks/useCart';
 import CartPage from "./pages/CartPage";
+import Layout from "./Layout";
 
 
 
 const Router = () => {
 
 
-    return<CartProvider >
-
+    return <CartProvider >
         <BrowserRouter>
             <Routes>
-                <Route path="/" index element={<Home />}></Route>
-                <Route path="/cart" element={<CartPage />}></Route>
+                <Route path="/" index element={
+                    <Layout>
+                        <Home />
+                    </Layout>
+                }></Route>
+                <Route path="/cart" element={
+                    <Layout>
+                        <CartPage />
+                    </Layout>
+                }></Route>
 
             </Routes>
         </BrowserRouter>
